235c37adc1b034323560f44de0957b6049bb122e
Martin Skow Røed <msroed@gmail.com>
2013-03-05 13:16:09 +0100

    fixed indentation, moved signalShouldStop = 0

--- elevator.c
+++ elevator.c
@@ -32,20 +32,20 @@ int initElevator() {
     }
     if (!initQueue())
         return 0;
-  directionUp = 1;
-  elev_set_speed(100);
-  while (elev_get_floor_sensor_signal()== -1) {
-    ;
-  }
-  stopElevator();
-  currentState = IDLE;
-  nextState = IDLE;
-  currentFloor = elev_get_floor_sensor_signal();
-  signalHasOrders = 0;
-  signalShouldStop = 0;
-  signalEmergencyStop = 0;
-  signalTimerIsFinished = 0;
-  signalObstruction = 0;
+    directionUp = 1;
+    elev_set_speed(100);
+    while (elev_get_floor_sensor_signal()== -1) {
+        ;
+    }
+    stopElevator();
+    currentState = IDLE;
+    nextState = IDLE;
+    currentFloor = elev_get_floor_sensor_signal();
+    signalHasOrders = 0;
+    signalShouldStop = 0;
+    signalEmergencyStop = 0;
+    signalTimerIsFinished = 0;
+    signalObstruction = 0;
     return 1;
 }
 
@@ -53,144 +53,145 @@ int main()
 {
     // Initialize hardware
     printf("Initializing elevator");
-
-
+    
+    
     initElevator();
     
     printf("Press STOP button to stop elevator and exit program.\n");
-      
+    
     while (1) {
-      switch (currentState) {
-      case IDLE:
-	if (signalHasOrders) nextState = DRIVE;
-	else nextState = IDLE;
-        break;
-
-      case DRIVE:
-	if (signalShouldStop) nextState = OPENDOOR;
-	else nextState = DRIVE;
-	break;
-      case OPENDOOR:
-	if (signalTimerIsFinished) nextState = CLOSEDOOR;
-	else nextState = OPENDOOR;
-	break;
-      case CLOSEDOOR:
-	if (signalObstruction) nextState = OPENDOOR;
-	else if (signalHasOrders) nextState = DRIVE;
-	else nextState = IDLE;
-	break;
-      case EMERGENCYSTOP:
-	if (signalHasOrders) nextState = DRIVE;
-	else nextState = EMERGENCYSTOP;
-	break;
-      }
-
-      if (signalEmergencyStop) nextState = EMERGENCYSTOP;
-
-      if (nextState != currentState) {
-	switch (nextState) {
-	case IDLE:
-	  break;
-	case DRIVE:
-      directionUp = findDirection();
-	  setSpeed();
-	  break;
-	case OPENDOOR:
-            if (currentState == DRIVE) {
-	  stopElevator();
-	  deleteOrderInFloor(currentFloor);
-	  turnOffLightsInFloor(currentFloor);
+        switch (currentState) {
+            case IDLE:
+                if (signalHasOrders) nextState = DRIVE;
+                else nextState = IDLE;
+                break;
+                
+            case DRIVE:
+                if (signalShouldStop) nextState = OPENDOOR;
+                else nextState = DRIVE;
+                break;
+            case OPENDOOR:
+                if (signalTimerIsFinished) nextState = CLOSEDOOR;
+                else nextState = OPENDOOR;
+                break;
+            case CLOSEDOOR:
+                if (signalObstruction) nextState = OPENDOOR;
+                else if (signalHasOrders) nextState = DRIVE;
+                else nextState = IDLE;
+                break;
+            case EMERGENCYSTOP:
+                if (signalHasOrders) nextState = DRIVE;
+                else nextState = EMERGENCYSTOP;
+                break;
+        }
+        
+        if (signalEmergencyStop) nextState = EMERGENCYSTOP;
+        
+        if (nextState != currentState) {
+            switch (nextState) {
+                case IDLE:
+                    break;
+                case DRIVE:
+                    directionUp = findDirection();
+                    setSpeed();
+                    break;
+                case OPENDOOR:
+                    if (currentState == DRIVE) {
+                        stopElevator();
+                        deleteOrderInFloor(currentFloor);
+                        turnOffLightsInFloor(currentFloor);
+                    }
+                    startTimer();
+                    break;
+                case CLOSEDOOR:
+                    break;
+                case EMERGENCYSTOP:
+                    stopElevator();
+                    deleteAllOrders();
+                    break;
             }
-	  startTimer();
-	  break;
-	case CLOSEDOOR:
-	  break;
-	case EMERGENCYSTOP:
-	  stopElevator();
-	  deleteAllOrders();
-	  break;
-	}
-      }
-
-      currentState = nextState;
-      updateSignals(currentState);
-      updatePanel();
-      printStatus();
+        }
+        
+        currentState = nextState;
+        updateSignals(currentState);
+        updatePanel();
+        printStatus();
         // Check if stop button, if so, stop elevator and exit program.
-      if (elev_get_stop_signal()) {
-	elev_set_speed(0);
-	break;
-      }
+        if (elev_get_stop_signal()) {
+            elev_set_speed(0);
+            break;
+        }
     }
     return 0;
 }
 
- void updateSignals(elevatorState curState) {
-   switch(curState) {
-   case IDLE:
-     signalHasOrders = hasOrders();
-     break;
-   case DRIVE:
-     if (elev_get_floor_sensor_signal() != -1) {
-       currentFloor = elev_get_floor_sensor_signal();
-         turnOnFloorLightIndicator(currentFloor);
-       if (hasOrderInFloor(directionUp, currentFloor) || (findDirection() == !directionUp)) signalShouldStop = 1;
-     }
-     else signalShouldStop = 0;
-     break;
-   case OPENDOOR:
-     signalTimerIsFinished = timerIsFinished(3);
-     break;
-   case CLOSEDOOR:
-     signalHasOrders = hasOrders();
-     signalObstruction = elev_get_obstruction_signal();
-     break;
-   case EMERGENCYSTOP:
-     break;
-   }
-   signalEmergencyStop=elev_get_stop_signal();
- }
+void updateSignals(elevatorState curState) {
+    switch(curState) {
+        case IDLE:
+            signalHasOrders = hasOrders();
+            break;
+        case DRIVE:
+            if (elev_get_floor_sensor_signal() != -1) {
+                currentFloor = elev_get_floor_sensor_signal();
+                turnOnFloorLightIndicator(currentFloor);
+                if (hasOrderInFloor(directionUp, currentFloor) || (findDirection() == !directionUp)) signalShouldStop = 1;
+                else signalShouldStop = 0;
+            }
+            else signalShouldStop = 0;
+            break;
+        case OPENDOOR:
+            signalTimerIsFinished = timerIsFinished(3);
+            //if (signalShouldStop) signalShouldStop = 0;
+            break;
+        case CLOSEDOOR:
+            signalHasOrders = hasOrders();
+            signalObstruction = elev_get_obstruction_signal();
+            break;
+        case EMERGENCYSTOP:
+            break;
+    }
+    signalEmergencyStop=elev_get_stop_signal();
+}
 
 void setSpeed(void) {
-   if (directionUp == UP)
-     elev_set_speed(300);
-   else elev_set_speed(-300);
- }
+    if (directionUp == UP)
+        elev_set_speed(300);
+    else elev_set_speed(-300);
+}
 
 int findDirection() {
-  int floor = currentFloor;
-  if (directionUp == UP) {
-    for (floor = floor+1; floor<N_FLOORS; floor++) {
-      if (hasOrderInFloor(UP, floor) || hasOrderInFloor(DOWN, floor))
-          return UP;
+    int floor = currentFloor;
+    if (directionUp == UP) {
+        for (floor = floor+1; floor<N_FLOORS; floor++) {
+            if (hasOrderInFloor(UP, floor) || hasOrderInFloor(DOWN, floor))
+                return UP;
+        }
+        return DOWN;
     }
-    return DOWN;
-  }
-  else if (directionUp == DOWN) {
-    for (floor = floor-1; floor>=0; floor--) {
-      if (hasOrderInFloor(UP, floor) || hasOrderInFloor(DOWN, floor))
-	    return DOWN;
+    else if (directionUp == DOWN) {
+        for (floor = floor-1; floor>=0; floor--) {
+            if (hasOrderInFloor(UP, floor) || hasOrderInFloor(DOWN, floor))
+                return DOWN;
+        }
+        return UP;
     }
+    printf("orderqueue error: direction neither up nor down");
     return UP;
-  }
-  printf("orderqueue error: direction neither up nor down");
-  return UP;
 }
 
 void stopElevator(){
-  if(directionUp==UP) elev_set_speed(-100);
-  else elev_set_speed(100);
-  usleep(50000);
-  elev_set_speed(0);
-    signalShouldStop = 0;
+    if(directionUp==UP) elev_set_speed(-100);
+    else elev_set_speed(100);
+    usleep(50000);
+    elev_set_speed(0);
 }
 
 int getCurrentFloor() {
-  return currentFloor;
+    return currentFloor;
 }
 
 void printStatus () {
-  printf("Current Floor: %i, HasOrders: %i, directionUP: %i, signalShouldStop = %i, currentState = %i, nextState = %i\n", currentFloor, hasOrders(), directionUp, signalShouldStop, currentState, nextState);
+    printf("Current Floor: %i, HasOrders: %i, directionUP: %i, signalShouldStop = %i, currentState = %i, nextState = %i\n", currentFloor, hasOrders(), directionUp, signalShouldStop, currentState, nextState);
 }
 
 
