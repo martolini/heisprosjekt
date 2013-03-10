0d0f046d5c76387c648c8a3e757afff73ebd2c18
Martin Skow Røed <msroed@gmail.com>
2013-03-10 23:16:34 +0100

    fix

--- timer.c
+++ timer.c
@@ -1,30 +1,18 @@
 #include "timer.h"
 #include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-#import <sys/time.h>
 
 
 static time_t theTime;
-static timeval *timeStart;
 
 void timer_start(void) {
     theTime = time(NULL);
-    gettimeofday(timeStart);
-    printf("timer started\n");
 }
 
 int timer_timerIsFinished(double sek) {
-    static timeval *timeNow;
-    gettimeofday(timeNow);
-    double diff = timeNow.tv_usec-timeStart.tv_usec;
-    if (diff >= 3)
+    static time_t timeNow;
+    timeNow = time(NULL);
+    printf("%f", difftime(timeNow, theTime));
+    if (difftime(timeNow, theTime) >= sek)
         return 1;
     return 0;
-//    static time_t timeNow;
-//    timeNow = time(NULL);
-//    printf("%f", difftime(timeNow, theTime));
-//    if (difftime(timeNow, theTime) >= sek)
-//        return 1;
-//    return 0;
 }
