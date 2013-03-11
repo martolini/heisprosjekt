92d4f4cfdf080313a64ce5642f843654ca7dfc68
Martin Skow Røed <msroed@gmail.com>
2013-03-11 04:23:18 +0100

    minor

--- timer.c
+++ timer.c
@@ -17,9 +17,10 @@ int timer_timerIsFinished(double sek) {
     static time_t timeNow;
     static struct timeval end;
     gettimeofday(&end, NULL);
-    double secs = end.tv_sec-start.tv_sec;
-    printf("%f", secs);
-    if (secs >= 3)
+    double secs = (end.tv_sec - start.tv_sec) * 1000
+    + (end.tv_usec - start.tv_usec) / 1000;
+    printf("%f\n", secs);
+    if (secs/1000 >= sek)
         return 1;
     return 0;
     
