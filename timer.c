11a9be1ccf5ed1b85ccc9f3ee67211bbeaa24177
Martin Skow Røed <msroed@gmail.com>
2013-03-11 04:28:47 +0100

    minor

--- timer.c
+++ timer.c
@@ -7,13 +7,12 @@
 static struct timeval startTime;
 
 void timer_start(void) {
-    theTime = time(NULL);
     gettimeofday(&startTime, NULL);
 }
 
 int timer_timerIsFinished(double sek) {
     static struct timeval endTime;
-    gettimeofday(&end, NULL);
+    gettimeofday(&endTime, NULL);
     double secs = (endTime.tv_sec - startTime.tv_sec) * 1000
     + (endTime.tv_usec - startTime.tv_usec) / 1000;
     if (secs/1000 >= sek)
