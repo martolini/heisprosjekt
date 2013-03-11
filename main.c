1c5f0ef2b44a414687662e96746c3a39ca3ede56
Martin Skow Røed <msroed@gmail.com>
2013-03-11 03:01:30 +0100

    main debug

--- main.c
+++ main.c
@@ -2,11 +2,13 @@
 // READ ELEV.H FOR INFORMATION ON HOW TO USE THE ELEVATOR FUNCTIONS.
 
 #include "elevator.h"
+#include "globals.h"
 #include <stdio.h>
 
 
 int main()
 {
+    printf("nos = %i", NUMBER_OF_SIGNALS);
     elev_run();
     return 0;
 }
